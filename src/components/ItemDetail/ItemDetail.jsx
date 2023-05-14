48398f4fb75959628b77e2cf7f4a593aa95f7677
Brenda Guardines <bguardines@gmail.com>
2023-05-13 21:12:17 -0300

    Subo readme modificado y elimino los lorems

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,13 +5,13 @@ import ItemCount from '../ItemCount/ItemCount';
 import { CarritoContext } from '../../context/CarritoContext';
 import { useContext } from 'react';
 
-const ItemDetail = ({ id, nombre, precio, img, stock }) => {
+const ItemDetail = ({ id, nombre, precio, img, stock, descripcion }) => {
 
     const [cantidadAgregada, setCantidadAgregada] = useState(0);
     const { agregarProducto } = useContext(CarritoContext);
     const manejadorCantidad = (cantidad) => {
         setCantidadAgregada(cantidad);
-        const item = { id, nombre, precio };
+        const item = { id, nombre, precio , descripcion};
         agregarProducto(item, cantidad);
     }
     return (
@@ -19,7 +19,7 @@ const ItemDetail = ({ id, nombre, precio, img, stock }) => {
             <img src={img} alt={nombre} />
             <h2>Nombre: {nombre} </h2>
             <h3>Id: {id} </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam iste reiciendis tempora unde! Quam commodi esse dicta odit nobis, enim, accusamus corrupti veritatis, excepturi aliquam laudantium? Veritatis ex sapiente deserunt.</p>
+            <p>{descripcion}</p>
             <h3>Precio: ${precio} </h3>
             {
                 cantidadAgregada > 0 ? (<Link to='/cart'> Terminar compra</Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />)
